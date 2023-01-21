dc90d220d5ee6064e0d14055c73f7cf84cb5cf8e
Charles Junior <selrahcjunior@hotmail.com>
2023-01-21 20:36:18 -0300

    style: adicionando transitions e focus nas interações do usuário

--- web-nlw/src/components/NewHabitForm/index.tsx
+++ web-nlw/src/components/NewHabitForm/index.tsx
@@ -38,6 +38,7 @@ export default function NewHabitForm(){
 	function handleToggleWeekDay(weekDay: number){
 		if(weekDays.includes(weekDay)){
 			const weekDaysWithRemovedOne = weekDays.filter(day => day !== weekDay);
+			setWeekDays(weekDaysWithRemovedOne);
 		}else{
 			const weekDaysWithAddedOne = [...weekDays, weekDay];
 			setWeekDays(weekDaysWithAddedOne);
@@ -48,14 +49,14 @@ export default function NewHabitForm(){
 		<form onSubmit={createNewHabit} className="w-full flex flex-col mt-6">
 			<label htmlFor="title" className="font-semibold leading-tight"> Qual seu comprometimento?</label>
 			<input type="text" id="title" placeholder="Ex: Exercícios, dormir bem, etc.."
-				className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400" autoFocus onChange={event => setTitle(event.target.value)} value={title}/>
+				className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-zinc-900" autoFocus onChange={event => setTitle(event.target.value)} value={title}/>
 			<label htmlFor="" className="font-semibold leading-tight">Qual a recorrência?</label>
 			<div className="flex flex-col gap-2 mt-3">
 				{
 					availableWeekDays.map((weekDay, index) => {
 						return(
-							<Checkbox.Root key={weekDay} className="flex items-center gap-3 group" onCheckedChange={() => handleToggleWeekDay(index)} checked={weekDays.includes(index)}>
-								<div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-violet-500 group-data-[state=checked]:border-violet-500">
+							<Checkbox.Root key={weekDay} className="flex items-center gap-3 group focus:outline-none" onCheckedChange={() => handleToggleWeekDay(index)} checked={weekDays.includes(index)}>
+								<div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-violet-500 group-data-[state=checked]:border-violet-500 transition-colors group-focus:ring-2 group-focus:ring-violet-700 group-focus:ring-offset-2 group-focus:ring-offset-zinc-900">
 									<Checkbox.Indicator>
 										<Check size={20} className="text-white"/>
 									</Checkbox.Indicator>
@@ -66,7 +67,7 @@ export default function NewHabitForm(){
 					})
 				}
 			</div>
-			<button type="submit" className="mt-6 rounded-lg p-4 flex items-center font-semibold bg-green-600 justify-center gap-3 hover:bg-green-500">
+			<button type="submit" className="mt-6 rounded-lg p-4 flex items-center font-semibold bg-green-600 justify-center gap-3 hover:bg-green-500 transition-colors focus:outline-none focus:ring-2 focus:ring-green-700 focus:ring-offset-2 focus:ring-offset-zinc-900">
 				<Check size={20} weight="bold"/>
 				Confirmar
 			</button>
--- web-nlw/src/components/ProgressBar/index.tsx
+++ web-nlw/src/components/ProgressBar/index.tsx
@@ -5,9 +5,9 @@ interface ProgressBarProps{
 export default function ProgressBar({ progress }: ProgressBarProps){
 
 	return(
-		<div className="h-2 rounded-xl bgzin w-full mt-4">
+		<div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
 			<div role="progressbar" aria-label="Progresso de hábitos completados nesse dia"
-				aria-valuenow={progress} className="h-3 rounded-xl bg-violet-600" style={{width: `${progress}%`}}/>
+				aria-valuenow={progress} className="h-3 rounded-xl bg-violet-600 transition-all" style={{width: `${progress}%`}}/>
 		</div>
 	);
 }
